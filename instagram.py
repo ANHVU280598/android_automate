0b9f1b343d26c72867f2d1416e4092911880710f
anhvu280598 <anhvu280598@gmail.com>
2025-03-26 23:14:58 -0400

    start code on instagram.py

--- instagram.py
+++ instagram.py
@@ -3,3 +3,21 @@ from automation_base.Base import Base
 
 if __name__ == '__main__':
     base = Base()
+    while True:
+        # CLick Kiem Thuong
+        path = '//android.widget.TextView[@text="Kiếm Thưởng"]'
+        base.click_on_element('XPATH', path)
+
+        # Click Instagram
+        path = '//android.widget.TextView[@text="Instagram"]'
+        base.click_on_element('XPATH', path)
+
+        # Click Nhan Job Ngay
+        path = '//android.widget.TextView[@text="Nhận Job ngay "]'
+        base.click_on_element('XPATH', path)
+
+    
+
+
+
+    
